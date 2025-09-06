6310fca5a0a7ec7c787e7c1d4589fb3a1a5a38b1
cateiru <git@cateiru.com>
2025-09-06 16:50:48 +0900

    クエリパラメータを設定可能に

--- internal/sesami/client.go
+++ internal/sesami/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/cateiru/sesami-2-mackerel/internal/config"
@@ -83,9 +84,21 @@ func (c *Client) GetDeviceStatus() (*DeviceStatus, error) {
 }
 
 func (c *Client) GetDeviceHistory() ([]HistoryEntry, error) {
-	url := fmt.Sprintf("https://app.candyhouse.co/api/sesame2/%s/history", c.DeviceUUID)
+	u, err := url.Parse(fmt.Sprintf("https://app.candyhouse.co/api/sesame2/%s/history", c.DeviceUUID))
+	if err != nil {
+		return nil, err
+	}
 
-	req, err := http.NewRequest("GET", url, nil)
+	query := u.Query()
+
+	// ページ数。ページ数は0から始まり、新しいものから古いものへの履歴の順番で、1ページに最大50件の履歴が含まれる。
+	query.Add("page", "0")
+	// 取得したい履歴のコンテンツ数。必要に応じて、新しいものから古いものへの履歴の順番で指定する。
+	query.Add("lg", "50")
+
+	u.RawQuery = query.Encode()
+
+	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
 		return nil, fmt.Errorf("履歴取得リクエスト作成エラー: %w", err)
 	}
